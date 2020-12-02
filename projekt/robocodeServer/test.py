1bf59be47626544ec344937e47a3bfd3cf6a288c
Petr M <muzosak@seznam.cz>
2020-12-02 19:11:06 +0100

    Add comments

--- projekt/robocodeServer/test.py
+++ projekt/robocodeServer/test.py
@@ -6,41 +6,57 @@ import matplotlib.pyplot as plt
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 dataset = pd.read_csv('data.csv')
-print(dataset.head(5))
-print(dataset.describe(include='all'))
+print(dataset.head(5)) # Returns first n rows
+print(dataset.describe(include='all')) # Generate various summary statistics, excluding NaN values.
 
 # print(sns.pairplot(dataset, hue='hit'))
 
-x = dataset.iloc[:,0:12]
+# creating input features and target variables
+x = dataset.iloc[:,0:12] # first argument: all rows; second argument: zero to twelve columns
 y = dataset.iloc[:,12]
 
+# standardize different input scales
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 x = sc.fit_transform(x)
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
 
 from keras import Sequential
 from keras.layers import Dense
 
-classifier = Sequential()
-#First Hidden Layer
-classifier.add(Dense(4, activation='relu', kernel_initializer='random_normal', input_dim=12)) #Second  Hidden Layer
-classifier.add(Dense(4, activation='relu', kernel_initializer='random_normal')) #Output Layer
-classifier.add(Dense(1, activation='sigmoid', kernel_initializer='random_normal'))
+model = Sequential() # Sequential is one of two main Keras models (the other one is Model)
 
-#Compiling the neural network
-classifier.compile(optimizer ='adam', loss='binary_crossentropy', metrics =['accuracy'])
+# Topology 1:
+# First Hidden Layer
+model.add(Dense(4, activation='relu', kernel_initializer='random_normal', input_dim=12))
+# Second  Hidden Layer
+model.add(Dense(4, activation='relu', kernel_initializer='random_normal'))
+# Output Layer
+model.add(Dense(1, activation='sigmoid', kernel_initializer='random_normal'))
 
-classifier.fit(x_train,y_train, batch_size=10, epochs=100)
+# Compiling the neural network
+# binary_crossentropy is used for calculation the loss function between actual output vs predicted output
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-eval_model=classifier.evaluate(x_train, y_train)
+# Fit the model
+model.fit(x_train, y_train, batch_size=5, epochs=100)
+
+# Return the loss value & metrics values for the model in test mode
+eval_model=model.evaluate(x_train, y_train)
+print("---------- eval_model --------------")
 print(eval_model)
 
-y_pred=classifier.predict(x_test)
-y_pred =(y_pred>0.5)
+# Predict output for our test dataset - make it a boolean based on its value
+y_pred=model.predict(x_test)
+y_pred=(y_pred>0.5)
 
+# Check the accuracy of NN
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+print("---------- confusion_matrix --------------")
+
+# left-upper and right-down are the values we want (true-positive and true-negative)
+# přidat i do prezentace
 print(cm)

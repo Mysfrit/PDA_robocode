1d58e4a4949e04e88ccd0ed1db73c51bf55a7671
Petr M <muzosak@seznam.cz>
2020-12-05 19:40:38 +0100

    Implement predicting mode

--- projekt/robocodeServer/server.py
+++ projekt/robocodeServer/server.py
@@ -6,12 +6,16 @@ def appendLinesToFile(lines):
     with open("data.csv", 'a') as file:
         file.write(str.join('\n', lines) + "\n")
 
+def loadLearnedModel():
+    from keras.models import load_model
+    return load_model('savedModel.h5')
+
 def listenForData():
     # get the hostname
     host = "localhost"
     port = 49000  # initiate port no above 1024
 
-    print("Starting Robocode Server...", host, port)
+    print("Starting Robocode NN Server in listening mode...", host, port)
 
     server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
@@ -40,17 +44,57 @@ def listenForData():
             continue
         
         # decode data and exclude first two characters (some random bytes)
-        decodedData = data.decode("utf-8", errors="replace")[2:]
+        decodedLine = data.decode("utf-8", errors="replace")[2:]
 
         # add to lines
-        lines.append(decodedData)
+        lines.append(decodedLine)
 
         # write to file every 100 new entries
         if i % 100 == 0:
             appendLinesToFile(lines)
             lines.clear()
 
+def listenForPredicting():
+    # get the hostname
+    host = "localhost"
+    port = 50000  # initiate port no above 1024
+
+    print("Starting Robocode NN Server in predicting mode...", host, port)
+
+    server_socket = socket.socket()  # get instance
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(5)
+
+    ourModel = loadLearnedModel()
+
+    while True:
+        # accept new connection
+        conn, address = server_socket.accept()
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            # if data is not received - continue
+            continue
+        
+        # decode data and exclude first two characters (some random bytes)
+        decodedLine = data.decode("utf-8", errors="replace")[2:]
+
+        # TODO: do something with decodedLine - ideally predict outcome using our NN 
+        outcome = ourModel.predict(decodedLine)
+
+        conn.send(outcome.encode("UTF-8")) # send outcome to the client
+        conn.close()
+
 if __name__ == '__main__':
     # change current working directory to the folder which contains this file
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    # listening mode
     listenForData()
+
+    # predicting mode
+    listenForPredicting()
+
